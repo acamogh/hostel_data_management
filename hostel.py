72230ffcbc1ae898b66ae6d6aab94b88f1e68cb6
acamogh <acamogh@gmail.com>
2015-11-08 11:42:04 -0800

    simple data entry page one connecting to db

--- hostel.py
+++ hostel.py
@@ -1,10 +1,14 @@
 from flask import send_file, g, session, Flask,render_template, request, jsonify, make_response, flash, redirect, url_for
 from functools import wraps
+import sqlite3
 
 import os, json
 app = Flask(__name__)
 
 app.secret_key = "1st_Hostel_data_managament !!"
+conn = sqlite3.connect("hostel.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS user_info (  register_number , student_name TEXT NOT NULL, college_name TEXT NOT NULL, home_town TEXT NOT NULL);")
 
 def byteify(input):
     if isinstance(input, dict):
@@ -41,16 +45,19 @@ def show_data():
 
 @app.route("/student_datails", methods=['POST'])
 def student_datails():
+    
+    reg_number=request.form['register_number']
     name=request.form['student_name']
-    number=request.form['register_number']
     coll_name=request.form['college_name']
     town=request.form['home_town']
 
-    print name,number,coll_name,town
+    print reg_number,name,coll_name,town
+
+    g.db = sqlite3.connect("hostel.db")
+    g.db.execute('insert into user_info (register_number, student_name, college_name, home_town) values (?, ?, ?, ?)',[reg_number, name, coll_name, town])   
+    g.db.commit()
+    g.db.close()
     return ('',203)
-
-
-
 
 
 @app.route("/logout")
